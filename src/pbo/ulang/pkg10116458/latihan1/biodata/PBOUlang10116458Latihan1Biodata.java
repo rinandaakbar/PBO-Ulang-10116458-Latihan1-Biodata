0f5588f0c2acdb846be915d6f25b896e55438376
nanda <nanda@nanda>
2019-09-26 19:02:06 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan1/biodata/PBOUlang10116458Latihan1Biodata.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : Rinanda Al Qorrie Akbar
+ * NIM      : 10116458
+ */
+public class PBOUlang10116458Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rinanda Al Qorrie Akbar");
+        System.out.println("ALAMAT\t\t: Sekeloa Tengah no 85");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 23 November 1997");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Lari");
+        System.out.println("MAKES\t\t: Nasgor");
+        System.out.println("MAKES\t\t: Kukubima");
+    }
+    
+}
